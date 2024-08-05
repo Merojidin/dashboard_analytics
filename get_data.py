badeab49a382685aaf93611c5f8996bd30d0427f
Merojidin <110535444+Merojidin@users.noreply.github.com>
2024-08-05 23:35:14 +0500

    Add files via upload

---
+++ get_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from connector import set_connection
+
+def read_query(query_name):
+    with open(f'queries/{query_name}.sql', 'r') as f:
+        return f.read()
+
+def get_data(query_name):
+    query = read_query(query_name)
+    with set_connection() as ps:
+        return pd.read_sql(query, ps)
+    
+def get_data_xlsx(csv_name):
+    file_path = f'source/{csv_name}.xlsx'
+    df = pd.read_excel(file_path)
+    df.columns = [col.lower().replace(' ', '').replace('-', '') for col in df.columns]
+    df = df.applymap(lambda x: x.strip().replace(',', '') if isinstance(x, str) else x)
+    return df
