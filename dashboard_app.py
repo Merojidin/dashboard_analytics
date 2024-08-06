fcf0977260a8461fbba08c43085f0d354695274e
Merojidin <110535444+Merojidin@users.noreply.github.com>
2024-08-06 16:58:16 +0500

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -264,7 +264,6 @@ def update_table(start_date, end_date, selected_categories):
     return sales.to_dict('records')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run_server(debug=True, host='0.0.0.0', port=port)
+    app.run_server(debug=True)
 
 
