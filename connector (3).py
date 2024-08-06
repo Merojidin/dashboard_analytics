00b00592d167da60039f5a3345178c1e8c779fd2
Merojidin <110535444+Merojidin@users.noreply.github.com>
2024-08-06 13:01:30 +0500

    Delete connector (3).py

--- connector (3).py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-
-db_string = 'postgresql+psycopg2://postgres:aziz22@localhost:5432/postgres'
-
-def set_connection():
-    engine = create_engine(db_string)
-    return engine.connect()
